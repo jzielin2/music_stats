83e54db44ce5557f6455b4a45d6f5adac3a67486
John Zielinski <jzielin2@gmail.com>
2015-08-30 18:23:25 -0400

    added Chart.js.  Now can create some chartz

---
+++ infoGraphic.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<script src="Chart.js/Chart.js"></script>
+		<script type="text/javascript">
+			var data = [
+			    {
+			        value: 300,
+			        color:"#F7464A",
+			        highlight: "#FF5A5E",
+			        label: "Red"
+			    },
+			    {
+			        value: 50,
+			        color: "#46BFBD",
+			        highlight: "#5AD3D1",
+			        label: "Green"
+			    },
+			    {
+			        value: 100,
+			        color: "#FDB45C",
+			        highlight: "#FFC870",
+			        label: "Yellow"
+			    }
+			];
+
+			function createChart(chartData, canvasID){
+				var ctx = document.getElementById(canvasID).getContext("2d");
+				var myDoughnutChart = new Chart(ctx).Doughnut(chartData,{});
+			}
+
+		</script>
+	</head>
+
+<body onload="createChart(data, 'myCanvas');">
+	<h1>Basic Chart Experimentation</h1>
+	<canvas id="myCanvas" width="400" height="400"></canvas>
+
+
+</body>
+
+
+</html>
